6a1ae543f5355809fa79fe2cbe59a968135fbf9d
Nikita Petrenko <nikita.petrenko@ynov.com>
2026-05-13 09:44:52 +0200

    change adm password

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,7 @@ import bcrypt from 'bcryptjs'
 const prisma = new PrismaClient()
 
 async function main() {
-  const password = await bcrypt.hash('ChangeMe123!', 12)
+  const password = await bcrypt.hash('Alexandre13@', 12)
   await prisma.adminUser.upsert({
     where: { email: 'admin@artdelatable.fr' },
     update: {},
